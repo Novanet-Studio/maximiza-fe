fa044f44cf598eea33ca327702a6dba8876e01ea
Davejs136 <davejs136@gmail.com>
2020-10-01 13:55:39 -0400

    refactor: layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,42 +7,17 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
-import "./layout.scss"
 
-// b
-// <div
-//   style={{
-//     margin: `0 auto`,
-//     maxWidth: 960,
-//     padding: `0 1.0875rem 1.45rem`,
-//   }}
-// >
-//   <main>{children}</main>
-// </div>
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header />
-      <main className="contenedor">{children}</main>
-      <Footer />
-    </>
-  )
-}
+const Layout = ({ children }) => (
+  <>
+    <Header />
+    <main className="contenedor">{children}</main>
+    <Footer />
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
