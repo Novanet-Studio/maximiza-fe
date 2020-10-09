033dcb7ff00e8dccd7fc96eac36a2d0fe70ac7af
Davejs136 <davejs136@gmail.com>
2020-10-09 17:10:57 -0400

    modal added

--- src/pages/servicios.js
+++ src/pages/servicios.js
@@ -3,58 +3,97 @@ import { graphql } from "gatsby"
 import Img from "gatsby-image"
 import LayoutInterno from "../components/layoutInterno"
 import SEO from "../components/seo"
+import Flecha from "../assets/images/dropdown.svg"
 import "./servicios.scss"
+import { useState } from "react"
 
-const Servicios = ({ data }) => (
-  <LayoutInterno>
-    <SEO title="Servicios" />
-    <section className="principal">
-      <div className="columna--left columna--left-movil">
-        <h1 className="principal__titulo">
-          {data.strapiServicios.principal.titulo}
-        </h1>
-        <p className="principal__texto">
-          {data.strapiServicios.principal.contenido}
-        </p>
-      </div>
-      <div className="columna--right columna--right-movil">
-        <Img
-          fluid={data.strapiServicios.principal.imagen.childImageSharp.fluid}
-        />
-      </div>
-    </section>
-
-    <section className="inversionista">
-      {data.strapiServicios.inversionista.map((item) => (
-        <div className="inversionista__grupo" key={item.id}>
-          <div className="inversionista__cabecera">
-            <h3 className="inversionista__titulo">{item.nombre}</h3>
-            <Img
-              className="inversionista__imagen"
-              fluid={item.imagen.childImageSharp.fluid}
-            />
+const Modal = ({ data, id, onClose }) =>
+  data.map(
+    (item) =>
+      item.id === id && (
+        <div className="modal">
+          <span onClick={onClose}>&times;</span>
+          <div className="modal__body">
+            <h2 className="modal__title">{item.titulo}</h2>
+            <p className="modal__content">{item.contenido}</p>
           </div>
-          <ul className="inversionista__contenido">
-            {item.items.map((element) => (
-              <li className="inversionista__item" key={element.id}>
-                <Img
-                  className="inversionista__icono"
-                  fluid={element.icono.childImageSharp.fluid}
-                />
-                <h4 className="inversionista__subtitulo">{element.titulo}</h4>
-                <p className="inversionista__texto">
-                <strong>{element.titulo}</strong>
-                  {element.contenido.substring(0, 150).concat("...")}
-                  <button className="inversionista__boton">Leer más</button>
-                </p>
-              </li>
-            ))}
-          </ul>
         </div>
-      ))}
-    </section>
-  </LayoutInterno>
-)
+      )
+  )
+
+const Servicios = ({ data }) => {
+  const [targetModal, setTargetModal] = useState('')
+
+  return (
+    <LayoutInterno>
+      <SEO title="Servicios" />
+      <section className="principal">
+        <div className="columna--left">
+          <h1 className="principal__titulo">
+            {data.strapiServicios.principal.titulo}
+          </h1>
+          <p className="principal__texto">
+            {data.strapiServicios.principal.contenido}
+          </p>
+        </div>
+        <div className="columna--right">
+          <Img
+            fluid={data.strapiServicios.principal.imagen.childImageSharp.fluid}
+          />
+        </div>
+      </section>
+      <section className="inversionista">
+        {data.strapiServicios.inversionista.map((item) => (
+          <div className="inversionista__grupo" key={item.id}>
+            <div className="inversionista__cabecera">
+              <h3 className="inversionista__titulo">{item.nombre}</h3>
+              <Img
+                className="inversionista__imagen"
+                fluid={item.imagen.childImageSharp.fluid}
+              />
+            </div>
+            <ul className="inversionista__contenido">
+              {item.items.map((element) => (
+                <li
+                  className="inversionista__item"
+                  key={element.id}
+                  data-id={element.id}
+                >
+                  <Img
+                    className="inversionista__icono"
+                    fluid={element.icono.childImageSharp.fluid}
+                  />
+                  <h4 className="inversionista__subtitulo">{element.titulo}</h4>
+                  <img
+                    className="inversionista__flecha"
+                    src={Flecha}
+                    alt="Desplegar más"
+                  />
+                  <p className="inversionista__texto">
+                    <strong>{element.titulo}</strong>
+                    {element.contenido}
+                    <button
+                      style={{
+                        width: "100%",
+                        cursor: "pointer",
+                        background: "green",
+                        color: "#fff",
+                      }}
+                      onClick={() => setTargetModal(element.id)}
+                    >
+                      Ver mas
+                    </button>
+                  </p>
+                </li>
+              ))}
+            </ul>
+            <Modal data={item.items} onClose={() => setTargetModal('')} id={targetModal} />
+          </div>
+        ))}
+      </section>
+    </LayoutInterno>
+  )
+}
 
 export default Servicios
 
