64c790d13de6da66c5addbbe2a61e5f07d3399fc
robertonovanet <rhernandez@novanet.studio>
2020-10-16 14:20:56 -0430

    Pagina 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,32 @@
 import React from "react"
-
-import Layout from "../components/layout"
+import { Link } from "gatsby"
+import LayoutInterno from "../components/layoutInterno"
 import SEO from "../components/seo"
+import Perdido from "../assets/images/max-404-sec1.jpg"
 
 const NotFoundPage = () => (
-  <Layout>
+  <LayoutInterno>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <section className="principal">
+      <div className="columna--left">
+        <img className="perdido__imagen" src={Perdido} alt="404" />
+      </div>
+
+      <div className="columna--right">
+        <h1 className="principal__titulo">Página no encontrada</h1>
+        <p className="principal__texto principal__texto--pequeño">
+          La página que estás intentando ver, no existe o ha sido cambiada de
+          lugar. Usa el menú de navegación para volver al sitio o haz click en
+          el botón de abajo para volver al inicio.
+        </p>
+        <button className="principal__button">
+          <Link to="/">Ir al inicio</Link>
+        </button>
+      </div>
+    </section>
+
+    <section className="perdido"></section>
+  </LayoutInterno>
 )
 
 export default NotFoundPage
