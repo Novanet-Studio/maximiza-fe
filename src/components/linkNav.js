113b3663ec8dc1f1868ccd1e5c79b2367de48c68
Davejs136 <davejs136@gmail.com>
2020-10-29 14:51:55 -0400

    refactor navigation links

---
+++ src/components/linkNav.js
@@ -0,0 +1,27 @@
+import React from "react"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+
+const linkActivo = {
+  fontWeight: 700,
+  color: "#00735f",
+}
+
+const linkStyles = {}
+
+const LinkNav = ({ text, direction = "right", to = "/" }) => (
+  <AniLink
+    className="cabecera__droplink"
+    cover
+    direction={direction}
+    duration={0.55}
+    bg="#006648"
+    to={to}
+    getProps={({ isPartiallyCurrent }) =>
+      isPartiallyCurrent ? { style: { ...linkStyles, ...linkActivo } } : null
+    }
+  >
+    {text}
+  </AniLink>
+)
+
+export default LinkNav
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,12 +1,5 @@
 import React from "react"
-import AniLink from "gatsby-plugin-transition-link/AniLink"
-
-const linkActivo = {
-  fontWeight: 700,
-  color: '#00735f',
-}
-
-const linkStyles = {}
+import LinkNav from "./linkNav"
 
 const navigation = () => (
   <nav id="myNav" className="overlay">
@@ -15,141 +8,27 @@ const navigation = () => (
         <p className="bala">•</p>
         <p className="texto">Nosotros</p>
         <div className="cabecera__menudrop">
-          <AniLink
-            className="cabecera__droplink"
-            cover
-            direction="right"
-            duration={0.55}
-            bg="#006648"
-            to="/empresa"
-            getProps={({ isPartiallyCurrent }) =>
-              isPartiallyCurrent
-                ? { style: { ...linkStyles, ...linkActivo } }
-                : null
-            }
-          >
-            Empresa
-          </AniLink>
-          <AniLink
-            className="cabecera__droplink"
-            cover
-            direction="left"
-            duration={0.55}
-            bg="#006648"
-            to="/servicios"
-            getProps={({ isPartiallyCurrent }) =>
-              isPartiallyCurrent
-                ? { style: { ...linkStyles, ...linkActivo } }
-                : null
-            }
-          >
-            Servicios
-          </AniLink>
-          <AniLink
-            className="cabecera__droplink"
-            cover
-            direction="right"
-            duration={0.55}
-            bg="#006648"
-            to="/contacto"
-            getProps={({ isPartiallyCurrent }) =>
-              isPartiallyCurrent
-                ? { style: { ...linkStyles, ...linkActivo } }
-                : null
-            }
-          >
-            Contacto
-          </AniLink>
+          <LinkNav text="Empresa" to="/empresa" />
+          <LinkNav text="Servicios" to="/servicios" direction="left" />
+          <LinkNav text="Contacto" to="/contacto" />
         </div>
       </div>
       <div className="cabecera__menugrupo">
         <p className="bala">•</p>
         <p className="texto">Impacto ESG</p>
         <div className="cabecera__menudrop">
-          <AniLink
-            className="cabecera__droplink"
-            cover
-            direction="right"
-            duration={0.55}
-            bg="#006648"
-            to="/capital"
-            getProps={({ isPartiallyCurrent }) =>
-              isPartiallyCurrent
-                ? { style: { ...linkStyles, ...linkActivo } }
-                : null
-            }
-          >
-            Private equity
-          </AniLink>
-          <AniLink
-            className="cabecera__droplink"
-            cover
-            direction="left"
-            duration={0.55}
-            bg="#006648"
-            to="/educacion"
-            getProps={({ isPartiallyCurrent }) =>
-              isPartiallyCurrent
-                ? { style: { ...linkStyles, ...linkActivo } }
-                : null
-            }
-          >
-            Educación
-          </AniLink>
-          <AniLink
-            className="cabecera__droplink"
-            cover
-            direction="right"
-            duration={0.55}
-            bg="#006648"
-            to="/responsabilidad"
-            getProps={({ isPartiallyCurrent }) =>
-              isPartiallyCurrent
-                ? { style: { ...linkStyles, ...linkActivo } }
-                : null
-            }
-          >
-            Responsabilidad
-          </AniLink>
+          <LinkNav text="Private equity" to="/capital" />
+          <LinkNav text="Educación" to="/educacion" direction="left" />
+          <LinkNav text="Responsabilidad" to="/responsabilidad" />
         </div>
       </div>
       <div className="cabecera__menugrupo">
         <p className="bala">•</p>
         <p className="texto">Comunidad</p>
         <div className="cabecera__menudrop">
-          <AniLink className="cabecera__droplink" to="/medios">
-            Medios
-          </AniLink>
-          <AniLink
-            className="cabecera__droplink"
-            cover
-            direction="right"
-            duration={0.55}
-            bg="#006648"
-            to="/blog"
-            getProps={({ isPartiallyCurrent }) =>
-              isPartiallyCurrent
-                ? { style: { ...linkStyles, ...linkActivo } }
-                : null
-            }
-          >
-            Blog
-          </AniLink>
-          <AniLink
-            className="cabecera__droplink"
-            cover
-            direction="right"
-            duration={0.55}
-            bg="#006648"
-            to="/legal"
-            getProps={({ isPartiallyCurrent }) =>
-              isPartiallyCurrent
-                ? { style: { ...linkStyles, ...linkActivo } }
-                : null
-            }
-          >
-            Legal
-          </AniLink>
+          <LinkNav text="Medios" to="/medios" />
+          <LinkNav text="Blog" to="/blog" />
+          <LinkNav text="Legal" to="/legal" />
         </div>
       </div>
     </div>
