ced681606310c6c4a39cc0293e56a70e6ea52a38
Davejs136 <davejs136@gmail.com>
2020-10-29 13:57:28 -0400

    remove logs

--- src/components/layoutIndex.js
+++ src/components/layoutIndex.js
@@ -11,29 +11,16 @@ const Layout = ({ children }) => {
     const nav = document.querySelector('#myNav')
     const main = document.querySelector("main")
 
-    console.log('It works toggle? (layoutIndex) ', isToggled)
-
-    console.log('-'.repeat(30))
-    console.log({
-      nav,
-      main,
-      toggle: isToggled
-    })
-
     if (!isToggled) {
       nav.style.width = "50%"
       main.style.opacity = 0.3
       setIsToggled(true)
-      console.log('Menu is open (LaIx)')
     } else {
       nav.style.width = "0%"
       main.style.opacity = 1
-      console.log('Menu is close (LaIx)')
       setIsToggled(false)
     }
 
-    console.log('end layoutIndex')
-    console.log('-'.repeat(30))
   }
 
   return (
