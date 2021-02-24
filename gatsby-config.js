231ad2d641340185d8358b28b3fc7a5cda400518
Freddy Polanía <fpolania@gmail.com>
2021-02-24 18:27:51 -0400

    Actualización GA4

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,7 +76,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "GA-263128202", // Google Analytics / GA        
+          "G-4W17JXKS6P", // Google Analytics / GA        
         ],
         gtagConfig: {
           optimize_id: "GTM-5LFLQGT",
