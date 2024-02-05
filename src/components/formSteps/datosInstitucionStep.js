6eca24ccd9aaee46da1f1b1b8e4bfa3859388e2d
David Arenas <davejs136@gmail.com>
2024-02-04 23:01:52 -0400

    refactor(dataStep): add label

--- src/components/formSteps/datosInstitucionStep.js
+++ src/components/formSteps/datosInstitucionStep.js
@@ -22,23 +22,41 @@ const DatosInstitucionStep = React.forwardRef((props, ref) => {
   return (
     <form className="steps-form">
       <div className="steps-form__group">
-        <input {...register('place', { required: true })} className="steps-form__input" type="text" placeholder="Lugar" />
-        <input {...register('productionDate', { required: true })} className="steps-form__input" type="text" placeholder="Fechas de elaboración" />
-        <input {...register('inverstorNumber', { required: true })} className="steps-form__input" type="text" placeholder="N° o código del inversionista" />
+        <div className="steps-form__group-item">
+          <label htmlFor="place">Lugar</label>
+          <input {...register('place', { required: true })} className="steps-form__input" type="text" placeholder="Lugar" />
+        </div>
+        <div className="steps-form__group-item">
+          <label htmlFor="productionDate">Fechas de elaboración</label>
+          <input {...register('productionDate', { required: true })} className="steps-form__input" type="text" placeholder="Fechas de elaboración" />
+        </div>
+        <div className="steps-form__group-item">
+          <label htmlFor="inverstorNumber">N° o código del inversionista</label>
+          <input {...register('inverstorNumber', { required: true })} className="steps-form__input" type="text" placeholder="N° o código del inversionista" />
+        </div>
       </div>
 
       <h4>Datos de la institución del sector valores</h4>
 
       <div className="steps-form__group">
-        <input
-          {...register('brokerageHouseName', { required: true })}
-          className="steps-form__input steps-form__input--2"
-          type="text"
-          placeholder="Nombre de casa de bolsa o sociedades de corretaje de valores"
-        />
-        <input {...register('taxInformationRegistration', { required: true })} className="steps-form__input" type="text" placeholder="Registro información fiscal" />
+        <div className="steps-form__group-item">
+          <label htmlFor="brokerageHouseName">Nombre de casa de bolsa o sociedades de corretaje de valores</label>
+          <input
+            {...register('brokerageHouseName', { required: true })}
+            className="steps-form__input steps-form__input--2"
+            type="text"
+          />
+        </div>
+
+        <div className="steps-form__group-item">
+          <label htmlFor="taxInformationRegistration">Registro información fiscal</label>
+          <input {...register('taxInformationRegistration', { required: true })} className="steps-form__input" type="text" placeholder="" />
+        </div>
       </div>
-      <input {...register('address', { required: true })}  className="steps-form__input" type="text" placeholder="Dirección" />
+      <div className="steps-form__group-item">
+        <label htmlFor="address">Dirección</label>
+        <input {...register('address', { required: true })}  className="steps-form__input" type="text" placeholder="Dirección" />
+      </div>
     </form>
   );
 })
