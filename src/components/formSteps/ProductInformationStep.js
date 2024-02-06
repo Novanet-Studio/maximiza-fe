226c6e4bdbdd1e92e1c243f63049a9106281ccc5
David Arenas <davejs136@gmail.com>
2024-02-06 14:15:20 -0400

    feat: fill form data from `productInformationStep`

--- src/components/formSteps/ProductInformationStep.js
+++ src/components/formSteps/ProductInformationStep.js
@@ -3,15 +3,17 @@ import { useForm } from "react-hook-form";
 import { useFormContext } from "../../context/formContext";
 
 const ProductInformationStep = React.forwardRef((props, ref) => {
-  const { register, trigger } = useForm();
-  const { nextStep } = useFormContext();
+  const { register, trigger, updateFormData } = useForm();
+  const { nextStep, getValues } = useFormContext();
 
 
   async function validate() {
-    const valid = await trigger()
+    const valid = await trigger();
+    const values = getValues();
     
     if (valid) {
-      nextStep()
+      updateFormData({ productInformation: values.productInformation });
+      nextStep();
     }
   }
 
@@ -27,15 +29,15 @@ const ProductInformationStep = React.forwardRef((props, ref) => {
       <div className="steps-form__group">
         <div className="steps-form__group-item">
           <label htmlFor="productName">Nombre del producto</label>
-          <input {...register('productName', { required: true })} className="steps-form__input" type="text" />
+          <input {...register('productInformation.productName', { required: true })} className="steps-form__input" type="text" />
         </div>
         <div className="steps-form__group-item">
           <label htmlFor="productAmount">Monto del producto adquirido</label>
-          <input {...register('productAmount', { required: true })} className="steps-form__input" type="text" />
+          <input {...register('productInformation.productAmount', { required: true })} className="steps-form__input" type="text" />
         </div>
         <div className="steps-form__group-item">
           <label htmlFor="currency">Moneda</label>
-          <input {...register('currency', { required: true })} className="steps-form__input" type="text" />
+          <input {...register('productInformation.currency', { required: true })} className="steps-form__input" type="text" />
         </div>
       </div>
   
@@ -45,56 +47,56 @@ const ProductInformationStep = React.forwardRef((props, ref) => {
       <div className="steps-form__group">
         <div className="steps-form__group-item">
           <label htmlFor="monthlyAmount">Monto promedio mensual</label>
-          <input {...register('monthlyAmount', { required: true })} className="steps-form__input" type="text" />
+          <input {...register('productInformation.monthlyAmount', { required: true })} className="steps-form__input" type="text" />
         </div>
         <div className="steps-form__group-item">
           <label htmlFor="numberOfTransactionsByTransfers">N° transacciones por transferencias</label>
-          <input {...register('numberOfTransactionsByTransfers', { required: true })} className="steps-form__input" type="text" />
+          <input {...register('productInformation.numberOfTransactionsByTransfers', { required: true })} className="steps-form__input" type="text" />
         </div>
         <div className="steps-form__group-item">
           <label htmlFor="sendOrReceiveFundsFromAbroad">Enviar o recibir fondos del exterior</label>
-          <input {...register('sendOrReceiveFundsFromAbroad', { required: true })} className="steps-form__input" type="text" />
+          <input {...register('productInformation.sendOrReceiveFundsFromAbroad', { required: true })} className="steps-form__input" type="text" />
         </div>
       </div>
   
       <div className="steps-form__group">
         <div className="steps-form__group-item">
           <label htmlFor="purchase">Compra</label>
-          <input {...register('purchase', { required: true })} className="steps-form__input" type="text" />
+          <input {...register('productInformation.purchase', { required: true })} className="steps-form__input" type="text" />
         </div>
         <div className="steps-form__group-item">
           <label htmlFor="sale">Venta</label>
-          <input {...register('sale', { required: true })} className="steps-form__input" type="text" />
+          <input {...register('productInformation.sale', { required: true })} className="steps-form__input" type="text" />
         </div>
         <div className="steps-form__group-item">
           <label htmlFor="originCountry">País origen</label>
-          <input {...register('originCountry', { required: true })} className="steps-form__input" type="text" />
+          <input {...register('productInformation.originCountry', { required: true })} className="steps-form__input" type="text" />
         </div>
       </div>
   
       <div className="steps-form__group">
         <div className="steps-form__group-item">
           <label htmlFor="destinationCountry">País destino</label>
-          <input {...register('destinationCountry', { required: true })} className="steps-form__input" type="text" />
+          <input {...register('productInformation.destinationCountry', { required: true })} className="steps-form__input" type="text" />
         </div>
         <div className="steps-form__group-item">
           <label htmlFor="virtualCurrencyUse">Uso moneda virtual</label>
-          <input {...register('virtualCurrencyUse', { required: true })} className="steps-form__input" type="text" />
+          <input {...register('productInformation.virtualCurrencyUse', { required: true })} className="steps-form__input" type="text" />
         </div>
       </div>
   
       <div className="steps-form__group">
         <div className="steps-form__group-item">
           <label htmlFor="">Motivos por los que solicita los servicios</label>
-          <input {...register('motives', { required: true })} className="steps-form__input" type="text" />
+          <input {...register('productInformation.motives', { required: true })} className="steps-form__input" type="text" />
         </div>
         <div className="steps-form__group-item">
           <label htmlFor="">Origen de los fondos</label>
-          <input {...register('fundsSource', { required: true })} className="steps-form__input" type="text" />
+          <input {...register('productInformation.fundsSource', { required: true })} className="steps-form__input" type="text" />
         </div>
         <div className="steps-form__group-item">
           <label htmlFor="">Destino de los fondos</label>
-          <input {...register('fundsDestination', { required: true })} className="steps-form__input" type="text" />
+          <input {...register('productInformation.fundsDestination', { required: true })} className="steps-form__input" type="text" />
         </div>
       </div>
   
