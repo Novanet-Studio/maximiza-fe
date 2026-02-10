799db18d783a8e3c35e3959ef740dcb60ca1bf32
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2026-02-10 11:37:55 -0400

    some change

--- app/components/common/Header.vue
+++ app/components/common/Header.vue
@@ -8,6 +8,7 @@ defineProps<{
   <header class="flex items-center h-20">
     <div :class="['w-full mx-auto px-4', estilo]">
       <NuxtLink to="/" class="block w-48 sm:w-56">
+        <!-- images -->
         <img
           class="w-full h-auto"
           src="/images/logo-maximiza.svg"
